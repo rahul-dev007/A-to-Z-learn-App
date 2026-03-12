536347532e4aa415b1b68de5a5f76962fdb744ed
Rahul Biswas <rahul114biswas@gmail.com>
2026-03-12 19:51:03 +0600

    fix htmlTree import path

--- app/components/HtmlTree.tsx
+++ app/components/HtmlTree.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { HtmlNode } from "@/lib/htmlTree";
+import { HtmlNode } from "@/lib/html/htmlTree";
 import { useState } from "react";
 import clsx from "clsx";
 
